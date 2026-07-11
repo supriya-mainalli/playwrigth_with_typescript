b74ebcd61f226723bd70181e038c136f9281c68b
Supriya Mainalli <supriyamainallio@gmail.com>
2026-07-11 15:45:53 +0530

    update data types

--- typescript_handson/01_intro.ts
+++ typescript_handson/01_intro.ts
@@ -21,3 +21,24 @@ console.log(age1 + name1) //28supriya
 5. void
 6. Union
 */
+
+let student1: any = "supriya";
+console.log(`assigning value ${student1}`);
+
+student1 = 20;
+console.log(`after updating value ${student1}`);
+
+
+// null types
+let names1: null = null
+console.log(names1)
+
+//union types
+let studentDetails: string | number | boolean;
+
+studentDetails = 1;
+console.log(studentDetails);
+studentDetails = "supriya"
+console.log(studentDetails);
+studentDetails = "Engineering"
+console.log(studentDetails);
